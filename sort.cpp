713c295f5a643aaed7b0120a71e6ff80e6a290e0
ThreeStones1029 <2320218115@qq.com>
2024-09-05 21:39:04 +0800

    update

--- sort.cpp
+++ sort.cpp
@@ -116,14 +116,14 @@ void merge_sort(std::vector<int>& array, int left, int right) {
 }
 
 // （6）快速排序
-int paritition(std::vector<int> array, int left, int right) {
+int paritition(std::vector<int>& array, int left, int right) {
     int pivot = array[left];
     while (left < right) {
-        while (left < right && array[right] > pivot) {
+        while (left < right && array[right] >= pivot) {
             --right;
         }
         array[left] = array[right];
-        while (left < right && array[left] >= pivot) {
+        while (left < right && array[left] <= pivot) {
             ++left;
         }
         array[right] = array[left];
@@ -136,17 +136,159 @@ void quick_sort(std::vector<int>& array, int left, int right) {
     if (left < right) {
         int pivot = paritition(array, left, right);
         quick_sort(array, left, pivot - 1);
-        quick_sort(array, pivot, right);
+        quick_sort(array, pivot + 1, right);
     }
 }
 
 // （7）堆排序
+// 维护大顶堆
+void max_heapify(std::vector<int>& array, int start, int end) {
+    int dad = start;
+    int son = dad * 2 + 1;
+    while (son <= end) { 
+        if (son + 1 <= end && array[son] < array[son + 1]) // 先比较两个子结点，选择其中最大的
+            son++;
+        if (array[dad] > array[son]) // 父结点大于这个最大的子结点，结束循环
+            return;
+        else {
+            std::swap(array[dad], array[son]); // 交换父子结点
+            dad = son;
+            son = dad * 2 + 1;
+        }
+    }
+}
+
+void heap_sort(std::vector<int>& array) {
+    int n = array.size();
+
+    // 构造大顶堆
+    for (int i = n / 2 - 1; i >= 0; i--)
+        max_heapify(array, i, n - 1);
+
+    // 开始排序
+    for (int i = n - 1; i > 0; i--) {
+        std::swap(array[0], array[i]);
+        max_heapify(array, 0, i - 1);
+    }
+}
 
 // （8）计数排序
 
+void counting_sort(std::vector<int>& array) {
+    int n = array.size();
+
+    int max = INT16_MIN, min = INT16_MAX;
+    // 找到数组里面的最大最小值
+    for (int i = 0; i < n; i++) {
+        max = max > array[i] ? max : array[i];
+        min = min < array[i] ? min : array[i];
+    }
+    // 创建计数数组，长度为最大值-最小值 + 1
+    std::vector<int> count_list;
+    int len = max - min + 1;
+    for (int i = 0; i < len; i++) {
+        count_list.push_back(0);
+    }
+    // 遍历数组，在对应位置自增
+    for (int i = 0; i < n; i++) {
+        count_list[array[i] - min]++;
+    }
+    // 重新排列原始数组
+    int index = 0;
+    for (int i = 0; i < len; i++) {
+        while (count_list[i]--) {
+            array[index++] = i + min;
+        }
+    }
+}
+
 // （9）桶排序
 
+void bucket_sort(std::vector<int>& array) {
+    int n = array.size();
+    if (n <= 1) return;
+
+    int max = INT16_MIN, min = INT16_MAX;
+    // 找到数组里面的最大最小值
+    for (int i = 0; i < n; i++) {
+        max = max > array[i] ? max : array[i];
+        min = min < array[i] ? min : array[i];
+    }
+
+    int bucket_size = max - min + 1;
+    // 桶的数量
+    int bucket_count = 10;
+
+    std::vector<std::vector<int>> bucket(bucket_count);
+
+    for (int num : array) {
+        int index = (num - min) * bucket_count / bucket_size;
+        bucket[index].push_back(num);
+    }
+
+    // 对每个桶的元素进行排序
+    for (auto& b: bucket) {
+        sort(b.begin(), b.end());
+    }
+
+    	// 将排序后的元素依次放回原数组
+	int index = 0;
+	for (auto& b : bucket) {
+		for (int num : b) {
+			array[index++] = num;
+		}
+    }
+}
+
+
 // （10）基数排序
+
+void radix_sort(std::vector<int>& array) {
+    // 先找到最大的数
+    int n = array.size();
+    int max = INT16_MIN;
+    // 找到数组里面的最大最小值
+    for (int i = 0; i < n; i++) {
+        max = max > array[i] ? max : array[i];
+    }
+    // 得到最大的数的位数
+    int d = 0;
+	while (max) {
+		max /= 10;
+		d++;
+	}
+    std::vector<int> count_list;
+
+    // 初始化十个桶
+    int *tmp = new int[n];
+    int *count = new int[10]; //计数器
+    int i, j, k;
+    int radix = 1;
+    for(i = 1; i <= d; i++) //进行d次排序
+    {
+        for(j = 0; j < 10; j++)
+            count[j] = 0; //每次分配前清空计数器
+        for(j = 0; j < n; j++)
+        {
+            k = (array[j] / radix) % 10; //统计每个桶中的记录数
+            count[k]++;
+        }
+        for(j = 1; j < 10; j++)
+            count[j] = count[j - 1] + count[j]; //将tmp中的位置依次分配给每个桶
+        for(j = n - 1; j >= 0; j--) //将所有桶中记录依次收集到tmp中
+        {
+            k = (array[j] / radix) % 10;
+            tmp[count[k] - 1] = array[j];
+            count[k]--;
+        }
+        for(j = 0; j < n; j++) //将临时数组的内容复制到data中
+            array[j] = tmp[j];
+        radix = radix * 10;
+    }
+    delete []tmp;
+    delete []count;
+}
+
 
 // 打印数组元素
 void print_array(std::vector<int>& array) {
@@ -158,7 +300,7 @@ void print_array(std::vector<int>& array) {
 
 
 int main() {
-    std::vector<int> array = {10, 20, 34, 35, -12, 89, 234, -45, 768, -456, 90, 343, 12, 340};
+    std::vector<int> array = {10, 20, 34, 35, 12, 89, 234, 45, 768, 456, 90, 343, 12, 340};
     // 排序前
     std::cout << "before sorting" << std::endl;
     print_array(array);
@@ -179,8 +321,20 @@ int main() {
     // merge_sort(array, 0, array.size() - 1);
     
     // 快速排序
-    quick_sort(array, 0, array.size());
- 
+    // quick_sort(array, 0, array.size() - 1);
+
+    // 堆排序
+    // heap_sort(array);
+
+    // 计数排序
+    // counting_sort(array);
+
+    // 桶排序
+    // bucket_sort(array);
+
+    // 基数排序
+    radix_sort(array);
+
     print_array(array);
 
 
---
+++ 科大讯飞1.cpp
@@ -0,0 +1,38 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: ThreeStones1029 2320218115@qq.com
+ * @Date: 2024-09-04 16:55:47
+ * @LastEditors: ShuaiLei
+ * @LastEditTime: 2024-09-05 14:25:55
+ */
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+
+
+void modify_list() {
+    
+}
+
+
+int main() {
+    int n, q;
+    std::cin >> n >> q;
+    int l, r;
+    std::vector<int> list;
+    // 初始化数组
+    for (int i = 1; i <= n; i++) {
+        list.push_back(i);
+    }
+    
+
+    while (q--) {
+        std::cin >> l >> r;
+
+        modify_list();
+
+    }
+    return 0;
+}
